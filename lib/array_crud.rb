91bf83eb28ac0c631de621df209c06755fb68e76
romeroraven <romeroravenr@gmail.com>
2020-11-13 12:29:53 -0700

    commit tree 69a97bfb01310075b585db4cc4db4c0ce8f324e7

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,58 @@
 def create_an_empty_array
-  
+    []
 end
 
 def create_an_array
-  
+
+   ["Beetle", "Spider", "Butterfly", "Fly"]
+
 end
 
 def add_element_to_end_of_array(array, element)
+    
+    expressions = ["Hi", "Hello", "Bye", "Ok"]
+  expressions.push ("arrays!")     
   
 end
 
 def add_element_to_start_of_array(array, element)
+
+    expressions = ["Hi", "Hello", "Bye", "Ok"]
+    expressions.unshift ("wow")  
   
 end
 
 def remove_element_from_end_of_array(array)
-  
+
+    expressions = ["Hi", "Hello", "Bye", "Ok", "arrays!"]
+  arrays_expressions = expressions.pop  
+
 end
 
 def remove_element_from_start_of_array(array)
-  
+
+    expressions = ["wow", "Hi", "Hello", "Bye", "Ok"]
+    wow = expressions.shift
+
 end
 
 def retrieve_element_from_index(array, index_number)
+
+    expressions = ["wow", "am", "Hi", "Hello", "Bye", "Ok"]
+    expressions [1]
   
 end
 
 def retrieve_first_element_from_array(array)
+
+    expressions = ["wow", "am", "Hi", "Hello", "Bye", "Ok"]
+    expressions [0]
   
 end
 
 def retrieve_last_element_from_array(array)
-  
+
+    expressions = ["wow", "am", "Hi", "Hello", "Bye", "Ok", "arrays!"]
+    expressions [-1]
+    
 end
